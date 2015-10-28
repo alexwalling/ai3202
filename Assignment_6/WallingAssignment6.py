d8eabae42c9d9a1e7b4560b7a3e741ab504d27e5
alexwalling <alex.walling19@gmail.com>
2015-10-28 15:23:23 -0600

    conditional nearly done

--- Assignment_6/WallingAssignment6.py
+++ Assignment_6/WallingAssignment6.py
@@ -2,7 +2,7 @@ import getopt
 import sys
 
 class Node():
-	def init(self):
+	def __init__(self):
 		self.parents = []
 		self.conditionals = {}
 
@@ -16,7 +16,7 @@ class Node():
 	def getParent(self):
 		return self.parents
 
-	def getConditionals(self):
+	def getConditional(self):
 		return self.conditionals
 	
 
@@ -27,7 +27,7 @@ def marginal(graph, args):
 		return graph["Smoker"].getConditional()["s"]
 	elif args is "C":
 		condition = graph["Cancer"].getConditional()
-		polution = graph["Pollution"].getConditional()["p"]
+		pollution = graph["Pollution"].getConditional()["p"]
 		smoker = graph["Smoker"].getConditional()["s"]
 		
 		p = (condition["ps"]*smoker) + (condition["p~s"]*(1-smoker))
@@ -36,15 +36,23 @@ def marginal(graph, args):
 		return (pollution*p) + ((1-pollution)*notp)
 	elif args is "X":
 		condition = graph["XRay"].getConditional()
-		cancer = marginal(graph, "C")[1]
+		cancer = marginal(graph, "C")#[1]
 		return (condition["c"]*cancer) + (condition["~c"]*(1-cancer))
 	elif args is "D":
 		condition = graph["Dyspnoea"].getConditional()
-		cancer = marginal(graph, "C")[1]
+		cancer = marginal(graph, "C")#[1]
 		return (condition["c"]*cancer) + (condition["~c"]*(1-cancer))
 
-def conditional():
-	pass
+def conditional(graph, x, y):
+	print "Calculating conditional probability of {0} given {1}".format(x, y)
+	if x is y:
+		return 1
+	else:#TODO
+		graph[y].setConditional(y, 1)
+		return calc_marginal(graph, x)
+
+	return None	
+
 def joint():
 	pass
 
@@ -57,26 +65,48 @@ if __name__ == "__main__":
 	network["XRay"] = Node()
 	network["Dyspnoea"] = Node()
 	
-	network["Cancer"].set_parents([network["Pollution"],network["Smoker"]])
-	network["XRay"].set_parents([network["Cancer"]])
-	network["Dyspnoea"].set_parents([network["Cancer"]])
+	network["Cancer"].setParents([network["Pollution"], network["Smoker"]])
+	network["XRay"].setParents([network["Cancer"]])
+	network["Dyspnoea"].setParents([network["Cancer"]])
 	
-	network["Pollution"].set_conditional("p",0.9)
+	network["Pollution"].setConditionals("p",0.9)
 	
-	network["Smoker"].set_conditional("s",0.3)
+	network["Smoker"].setConditionals("s",0.3)
 	
-	network["Cancer"].set_conditional("~ps",0.05)
-	network["Cancer"].set_conditional("~p~s",0.02)
-	network["Cancer"].set_conditional("ps",0.03)
-	network["Cancer"].set_conditional("p~s",0.001)
+	network["Cancer"].setConditionals("~ps",0.05)
+	network["Cancer"].setConditionals("~p~s",0.02)
+	network["Cancer"].setConditionals("ps",0.03)
+	network["Cancer"].setConditionals("p~s",0.001)
 	
-	network["Cancer"].set_conditional("s~p",0.05)
-	network["Cancer"].set_conditional("~s~p",0.02)
-	network["Cancer"].set_conditional("sp",0.03)
-	network["Cancer"].set_conditional("~sp",0.001)
+	network["Cancer"].setConditionals("s~p",0.05)
+	network["Cancer"].setConditionals("~s~p",0.02)
+	network["Cancer"].setConditionals("sp",0.03)
+	network["Cancer"].setConditionals("~sp",0.001)
 	
-	network["XRay"].set_conditional("c",0.9)
-	network["XRay"].set_conditional("~c",0.2)
+	network["XRay"].setConditionals("c",0.9)
+	network["XRay"].setConditionals("~c",0.2)
 	
-	network["Dyspnoea"].set_conditional("c",0.65)
-	network["Dyspnoea"].set_conditional("~c",0.3)
+	network["Dyspnoea"].setConditionals("c",0.65)
+	network["Dyspnoea"].setConditionals("~c",0.3)
+
+
+	try:
+		opts, args = getopt.getopt(sys.argv[1:], "m:g:j:p:")
+	except getopt.GetoptError as err:
+		print(str(err))
+		sys.exit(2)
+	for o, a in opts:
+		if o in ("-p"):
+			if a[0] == "P":
+				network["Pollution"].set_conditional("p",float(a[1:]))
+			elif a[0] == "S":
+				network["Smoker"].set_conditional("s",float(a[1:]))
+		elif o in ("-m"):
+			print(marginal(network, a))
+		elif o in ("-g"):
+			(var, given) = a.split('|')
+			conditional(network, var, given)
+		elif o in ("-j"):
+			joint(network, a)
+		else:
+			assert False, "unhandled option"
